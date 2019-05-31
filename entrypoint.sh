f2170581a56dbdbfc66a60abf1ddd1dea130a4a2
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2019-05-31 09:26:46 +0900

    create link to workspace folder to home folder as well

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,8 @@ GROUP_ID=$(id -g)
 sudo usermod -u $USER_ID -o -m -d /home/developer developer > /dev/null 2>&1
 sudo groupmod -g $GROUP_ID developer > /dev/null 2>&1
 
+if [ ! -e "/home/developer/workspace" ]; then
+    ln -s /workspace /home/developer
+fi
+
 exec $@
