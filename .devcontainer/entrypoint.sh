8608d48bbdf13627af828a3316bdec89acfb82fc
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2019-08-12 10:55:06 +0900

    enable default xml completion on theia

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -6,7 +6,9 @@ GROUP_ID=$(id -g)
 sudo usermod -u $USER_ID -o -m -d /home/developer developer > /dev/null 2>&1
 sudo groupmod -g $GROUP_ID developer > /dev/null 2>&1
 
-ln -sfn /workspace /home/developer
+ln -sfn /workspace /home/developer/workspace
+
+ln -sfn /workspace/.vscode /home/developer/.theia
 
 source /opt/ros/kinetic/setup.bash
 
