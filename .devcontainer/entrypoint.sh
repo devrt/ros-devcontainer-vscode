93c3a98e87f5d83406d72daa763274f8a27d2c74
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2019-08-13 15:42:10 +0900

    use docker volume to persist workspace data to avoid permission issue

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -5,11 +5,16 @@ GROUP_ID=$(id -g)
 
 sudo usermod -u $USER_ID -o -m -d /home/developer developer > /dev/null 2>&1
 sudo groupmod -g $GROUP_ID developer > /dev/null 2>&1
+sudo chown -R developer:developer /workspace
+
+ln -sfn /home/developer/.vscode /workspace/.vscode
 
 ln -sfn /workspace /home/developer/workspace
 
-ln -sfn /workspace/.vscode /home/developer/.theia
-
 source /opt/ros/kinetic/setup.bash
+
+mkdir -p /workspace/src && cd /workspace/src && catkin_init_workspace || true
+
+cd /home/developer
 
 exec $@
